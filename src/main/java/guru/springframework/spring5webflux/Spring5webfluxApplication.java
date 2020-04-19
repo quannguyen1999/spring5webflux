42e89e74e6adbe0ef6842596a48bcffc194f585c
quannguyen1999 <nguyendanganhquan99@gmail.com>
2020-04-19 09:45:07 +0100

    init commit:basic spring spring webflux.

---
+++ src/main/java/guru/springframework/spring5webflux/Spring5webfluxApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5webfluxApplication.class, args);
+    }
+
+}
